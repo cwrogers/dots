430c3975f6d3de0fad9116f97021d6b28cdf7f93
crogers <cwrogiv@gmail.com>
2021-10-09 22:05:39 -0700

    added wal colors

---
+++ wal/wal/colors-tty.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+[ "${TERM:-none}" = "linux" ] && \
+    printf '%b' '\e]P01f2c37
+                 \e]P12d8bb7
+                 \e]P23fa2cf
+                 \e]P354abd4
+                 \e]P455bad4
+                 \e]P5e39192
+                 \e]P68cdae2
+                 \e]P7c6c6c7
+                 \e]P8545557
+                 \e]P92d8bb7
+                 \e]PA3fa2cf
+                 \e]PB54abd4
+                 \e]PC55bad4
+                 \e]PDe39192
+                 \e]PE8cdae2
+                 \e]PFc6c6c7
+                 \ec'
---
+++ wal/wal/colors-wal-dmenu.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#c6c6c7", "#1f2c37" },
+	[SchemeSel] = { "#c6c6c7", "#2d8bb7" },
+	[SchemeOut] = { "#c6c6c7", "#8cdae2" },
+};
